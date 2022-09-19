93cb69b5173ed055e6c8f966d9a5b6418577165b
NickHeinzinger <nicholas.heinzinger@vtsd.com>
2022-09-19 09:56:15 -0400

    100 Point Version

--- Lab03vst.java
+++ Lab03vst.java
@@ -8,8 +8,16 @@ public class Lab03vst
 {
 	public static void main(String[] args)
 	{
-      System.out.println("Lab03, 80 Point Version\n");
-      
+      System.out.println("Lab03, 100 Point Version\n");
+      int milli = 10000123;
+	  System.out.println ("Starting seconds:" +milli);
+	  System.out.println("Hours:" +milli/3600000);
+	  int millisecleftafthr = milli%3600000;
+	  System.out.println("Minutes:" +millisecleftafthr/60000);
+	  int millisecleftaftmin = millisecleftafthr%60000;
+	  System.out.println("Seconds:" +millisecleftaftmin/1000);
+	  System.out.println("Milli Seconds:" +millisecleftaftmin%1000);
+
 	}
 }
 
